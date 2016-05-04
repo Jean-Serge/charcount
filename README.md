e810afeb12f4f29a364cb56b524c67f5d4ab9cec
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-05-04 17:01:18 +0200

    Charcount implementation

---
+++ README.md
@@ -0,0 +1,16 @@
+# Char Count in Rust
+
+Simple version of char count implementation for [atelier-prog](http://atelier-prog.github.io/topics/charcount.html).
+
+This program count the occurency of each character in a given string.
+
+## Usage
+
+`cargo run -- "your string"`
+
+*Note : for instance, the " are needed*
+
+## TODO
++ [ ] Count char occurency in a given string
++ [ ] Accept any number of parameters
++ [ ] Correctly handle error
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut chars: [u8; 128] = [0; 128];        // Initialize an array of 0 for counting chars appearance
+    let mut it = env::args();
+    it.next();
+    let string = it.next().unwrap();      // Get the given string from command line
+
+    // Count char occurency
+    for c in string.into_bytes() {
+        chars[c as usize] = chars[c as usize] + 1;
+    }
+
+    // Print char count
+    for i in 0..chars.len() {
+        if chars[i] == 0 {
+            continue
+        }
+        println!("{0} appeared {1} times in the string.", i as u8 as char, chars[i]);
+    }
 }
