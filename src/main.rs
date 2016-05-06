1ac5c1866ccbd73979d081eaa9c98882d96e93da
Jean-Serge Monbailly <j.monbailly@orange.fr>
2016-05-06 09:23:02 +0200

    Split program in functions and handle error when no argument is given to it.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 use std::env;
 
-fn main() {
+fn char_count(s: String) -> [u8; 128] {
     let mut chars: [u8; 128] = [0; 128];    // Initialize an array of 0 for counting chars appearance
-    let mut it = env::args();
-
-    it.next();                              // Ignore the program name
-    let string = it.next().unwrap();        // Get the given string from command line
 
     // Count char occurency
-    for c in string.into_bytes() {
+    for c in s.into_bytes() {
         chars[c as usize] = chars[c as usize] + 1;
     }
 
+    chars
+}
+
+fn print_char_count(t: [u8; 128]) -> () {
     // Print char count
-    for i in 0..chars.len() {
-        if chars[i] == 0 {
+    for i in 0..t.len() {
+        if t[i] == 0 {
             continue
         }
-        println!("{0} appeared {1} times in the string.", i as u8 as char, chars[i]);
+        println!("{0} appears {1} time.", i as u8 as char, t[i]);
+    }
+}
+
+fn main() {
+    let mut it = env::args();
+    it.next();                          // Ignore the program name
+
+    match it.next() {
+        Some(string) => print_char_count(char_count(string)),
+        None            => println!("Too few arguments to run this program.")
     }
 }
